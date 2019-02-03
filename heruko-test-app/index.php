b056921249968941b91159592381ea43b0fa1b8a
Shomari Simpson <100581869@oncampus.local>
2019-02-03 18:54:07 -0500

    Update index.php

--- heruko-test-app/index.php
+++ heruko-test-app/index.php
@@ -5,6 +5,7 @@ $config = [
         "key" => getenv('AWS_ACCESS_KEY_ID'),           // Fill in with your AWS Key
         "secret" => getenv('AWS_SECRET_ACCESS_KEY'),        // Fill in with your AWS Secret
         "region" => getenv('AWS_REGION'),  // Fill in with your S3 Bucket's region
+        "session" =>getenv('AWS_SESSION_TOKEN'),
         "version" => "2019-02-03",
     ];
 // this will simply read AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY from env vars
