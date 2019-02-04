19c5f30133c446611e73439053494691a223cc65
Shomari Simpson <100581869@oncampus.local>
2019-02-03 19:10:01 -0500

    Update index.php

--- heruko-test-app/index.php
+++ heruko-test-app/index.php
@@ -15,9 +15,14 @@ $config = [
         "version" => "2019-02-03",
     ];
 // this will simply read AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY from env vars
-$s3 = Aws\S3\S3Client::factory();
-//$s3 = Aws\S3\S3Client::factory($config);
-$bucket = getenv('S3_BUCKET')?: die('No "S3_BUCKET" config var in found in env!');
+//$s3 = Aws\S3\S3Client::factory();
+$s3 = S3Client::factory(array(
+    'credentials' => array(
+        'key'    => $ke,
+        'secret' => $se,
+        'token'  => $sess,
+    )
+));$bucket = getenv('S3_BUCKET')?: die('No "S3_BUCKET" config var in found in env!');
 ?>
 <html>
     <head><meta charset="UTF-8"></head>
