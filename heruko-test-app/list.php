874980afffd8e9b0f1ba0988a82fcf295939a308
Shomari Simpson <100581869@oncampus.local>
2019-02-05 15:54:00 -0500

    Update list.php

--- heruko-test-app/list.php
+++ heruko-test-app/list.php
@@ -169,8 +169,11 @@ $objects = $s3->getIterator('ListObjects', array(
 ));
 foreach ($objects as $object) {
 ?>
+
 <p> <a href="<?=htmlspecialchars($s3->getObjectUrl($bucket, $object['Key']))?>"> <?echo $object['Key'] . "<br>";?></a></p>
-<? echo "<script> drawDataURIOnCanvas(".htmlspecialchars($s3->getObjectUrl($bucket, $object['Key'])).", canvas); </script>"; ?>
+<canvas id="canvas<?=htmlspecialchars($s3->getObjectUrl($bucket, $object['Key']))?>" width="640" height="480"></canvas>
+
+<? echo "<script> drawDataURIOnCanvas("?><?=htmlspecialchars($s3->getObjectUrl($bucket, $object['Key']))?><?echo ", canvas"?><?=htmlspecialchars($s3->getObjectUrl($bucket, $object['Key']))?> <? echo "); </script>"; ?>
 <?		}?>
 
 <?php } catch(Exception $e) {
