96806172e37e03812aa381f3e7c3317f7dfc1312
Shomari Simpson <100581869@oncampus.local>
2019-02-06 11:03:13 -0500

    Update list.php

--- heruko-test-app/list.php
+++ heruko-test-app/list.php
@@ -124,7 +124,9 @@ $bucket = getenv('S3_BUCKET')?: die('No "S3_BUCKET" config var in found in env!'
         //imgg.src = url;
         var canvasID = "canvas"+i;
         var canvas = document.getElementById(canvasID);
-        imgg.crossOrigin = "Anonymous";
+        //imgg.crossOrigin = "Anonymous";
+        imgg.crossOrigin = "Use-Credentials";
+
         imgg.src = canvas.toDataURL();
         var jpg = true;
         try {
