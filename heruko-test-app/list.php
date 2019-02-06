68388c8bfc7493322e43563dd9ad1f8e7ecdc710
Shomari Simpson <100581869@oncampus.local>
2019-02-06 11:31:33 -0500

    Update list.php

--- heruko-test-app/list.php
+++ heruko-test-app/list.php
@@ -186,7 +186,7 @@ foreach ($objects as $object) {
 ?>
 
 <p> <a href="<?=$url?>"> <?echo $object['Key'] . "<br>";?></a></p>
-<canvas id="canvas<?=$i?>" width="640" height="480" crossorigin="anonymous"></canvas>
+<canvas id="canvas<?=$i?>" width="640" height="480" crossorigin="Anonymous"></canvas>
 <button type="button" id="button<?=$i?>">Click to view image</button>
 <button type="button" id="analyze<?=$i?>">Click to analyze image</button>
 <p id="opResult<?=$i?>"></p>
